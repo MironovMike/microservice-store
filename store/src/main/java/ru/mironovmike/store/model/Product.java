3f627b7b1e52f585d6d6eccba9df330d97ae1e8c
Mikhail Mironov <mail@mironovmike.ru>
2022-09-21 10:26:54 +0300

    Basic store app setup with Spring Cloud Config usage

---
+++ store/src/main/java/ru/mironovmike/store/model/Product.java
@@ -0,0 +1,16 @@
+package ru.mironovmike.store.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    @NotNull
+    private String title;
+}
---
+++ store/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO products(title)  VALUES('Audi');
+INSERT INTO products(title)  VALUES('BMW');
