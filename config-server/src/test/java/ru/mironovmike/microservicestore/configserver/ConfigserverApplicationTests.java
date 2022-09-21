805466037ac5647f6ecbf4b4f1d6659e180111a7
Mikhail Mironov <mail@mironovmike.ru>
2022-09-21 10:25:49 +0300

    Basic config-server setup with encrypted properties

---
+++ config-server/src/test/java/ru/mironovmike/microservicestore/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mironovmike.microservicestore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
