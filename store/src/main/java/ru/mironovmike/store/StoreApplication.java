edcfaa59fe9a2ebbc93aee57aaeecfa8a5ccab9c
Mikhail Mironov <mail@mironovmike.ru>
2022-09-24 19:02:58 +0300

    Localization base setup

--- store/src/main/java/ru/mironovmike/store/StoreApplication.java
+++ store/src/main/java/ru/mironovmike/store/StoreApplication.java
@@ -4,6 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @RefreshScope
@@ -11,5 +17,20 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 public class StoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		Locale russian = new Locale("ru");
+		localeResolver.setDefaultLocale(russian);
+		return localeResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasenames("messages");
+		return messageSource;
 	}
 }
--- store/src/main/java/ru/mironovmike/store/service/ProductService.java
+++ store/src/main/java/ru/mironovmike/store/service/ProductService.java
@@ -2,7 +2,9 @@ package ru.mironovmike.store.service;
 
 import ru.mironovmike.store.entity.Product;
 
+import java.util.Locale;
+
 public interface ProductService {
     Product create(Product product);
-    Product findById(long id);
+    Product findById(long id, Locale locale);
 }
