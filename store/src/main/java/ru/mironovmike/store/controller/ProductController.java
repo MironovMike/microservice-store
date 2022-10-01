109e33378871907f950096892a5750b218275c92
Mikhail Mironov <mail@mironovmike.ru>
2022-10-01 10:01:51 +0300

    Store controller get method specified

--- store/src/main/java/ru/mironovmike/store/controller/ProductController.java
+++ store/src/main/java/ru/mironovmike/store/controller/ProductController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.mironovmike.store.entity.Product;
 import ru.mironovmike.store.service.ProductService;
 
@@ -21,7 +18,7 @@ public class ProductController {
     @Autowired
     private final ProductService productService;
 
-    @RequestMapping(value = "/{productId}")
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
     public ResponseEntity<Product> getProduct(@PathVariable("productId") long productId,
                                               @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
         return ResponseEntity.ok(productService.findById(productId, locale));
