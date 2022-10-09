de03cae2f710f2764adf8a8e9ba3b33044d2157c
Mikhail Mironov <mail@mironovmike.ru>
2022-10-09 12:40:15 +0300

    Database fill event changed

--- store/src/main/java/ru/mironovmike/store/util/ApplicationStartingEventListener.java
+++ store/src/main/java/ru/mironovmike/store/util/ApplicationStartingEventListener.java
@@ -3,8 +3,8 @@ package ru.mironovmike.store.util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import ru.mironovmike.store.entity.Product;
 import ru.mironovmike.store.service.ProductService;
@@ -14,12 +14,12 @@ import java.util.Currency;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class ApplicationStartingEventListener implements ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationStartingEventListener implements ApplicationListener<ApplicationStartedEvent> {
     @Autowired
     private final ProductService productService;
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ApplicationStartedEvent event) {
         log.info("Run ContextRefreshedEvent handler");
         MonetaryAmount monetaryAmount = MonetaryAmount.builder().price(150d).currency(Currency.getInstance("RUB")).build();
         Product product = Product.builder()
