2cd0a3f865496b5591c312a337c2503fff850dbd
Mikhail Mironov <mail@mironovmike.ru>
2022-10-16 14:15:31 +0300

    Set default currency for empty Accept-Language header

--- store/src/main/java/ru/mironovmike/store/util/LocaleCurrencyResolver.java
+++ store/src/main/java/ru/mironovmike/store/util/LocaleCurrencyResolver.java
@@ -3,15 +3,15 @@ package ru.mironovmike.store.util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.Currency;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 @Component
 @Slf4j
 public class LocaleCurrencyResolver {
     public Currency getCurrency(Locale locale) {
+        if (locale == null) {
+            return Currency.getInstance("RUB");
+        }
         Map<String, Currency> map = new HashMap<>();
         map.put(Locale.ENGLISH.getLanguage(), Currency.getInstance("USD"));
         return map.getOrDefault(locale.getLanguage(), Currency.getInstance("RUB"));
